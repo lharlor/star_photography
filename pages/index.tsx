d9c37f9e932d7fa8cc5ba10c1b558472fb3a38c8
Lucas Harlor <lucasharlor@gmail.com>
2023-03-13 17:16:12 -0700

    Connecting to supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,15 @@
 import Image from 'next/image';
+import { useState } from 'react'
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+);
+
+function cn(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function Gallery() {
     return (
@@ -14,6 +25,8 @@ export default function Gallery() {
 }
 
 function BlurImage() {
+    const [isLoading, setLoading] = useState(true);
+
     return (
         <a href="#" className="group">
             <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full
@@ -23,7 +36,13 @@ function BlurImage() {
                     src="https://bit.ly/placeholder-img"
                     layout="fill"
                     objectFit="cover"
-                    className="group-hover:opacity-75"
+                    className={cn(
+                        "group-hover:opacity-75 duration-700 ease-in-out",
+                        isLoading
+                            ? 'grayscale blur-2xl scale-110'
+                            : 'grayscale-0 blur-0 scale-100'
+                    )}
+                    onLoadingComplete={()=>setLoading(false) }
                 />
             </div>
             <h3 className="mt-4 text-sm text-gray-700">Lucas harlor</h3>
