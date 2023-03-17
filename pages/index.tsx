acc0a87affa3485c48a87305cde745c41870d60d
Lucas Harlor <lucasharlor@gmail.com>
2023-03-16 17:11:54 -0700

    Got image data into index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import FileObject from 'next/app';
 import { useState } from 'react'
 import { getAllImageData } from '../lib/images'
 import Link from 'next/link';
@@ -6,10 +7,11 @@ import Link from 'next/link';
 
 export async function getStaticProps() {
     const allImageData = getAllImageData();
-
+    const resolved = await allImageData;
+    console.log(resolved);
     return {
         props: {
-            allImageData,
+            allImageData: resolved,
         },
     }
 }
@@ -25,15 +27,14 @@ type Image = {
 
 }
 
-export default function Gallery({ allImageData }: { allImageData: Image[]}) {
+export default function Gallery({ allImageData }: { allImageData: FileObject[] }) {
+    console.log(allImageData);
     return (
        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24
         sm:px-6 lg:max-w-7xl lg:px-8">
             <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2
             gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                {allImageData.map((image) => (
-                    <Link href={`/images/${image}`}></Link>
-                ))}
+
             </div>
         </div> 
     );
