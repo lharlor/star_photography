e0e8312caf604d5f269198311cce838d714f18f0
Lucas Harlor <lucasharlor@gmail.com>
2023-03-26 16:21:32 -0700

    Change source to table data instead of storage meta data

--- pages/images/[name].tsx
+++ pages/images/[name].tsx
@@ -27,6 +27,6 @@ export async function getStaticPaths() {
 
 export default function BlurImage({ imageData }) {
     return (
-        <a>imageData</a>
+        <a>{imageData}</a>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import FileObject from 'next/app';
-import { getImageData,getAllImageData } from '../lib/images'
+import { getImageInfo,getAllImageData } from '../lib/images'
 import { useState } from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
 
 
 export async function getStaticProps() {
-    const allImageData = getAllImageData();
-    const resolved = await allImageData;
+    const imageInfo = getImageInfo();
+    const resolved = await imageInfo;
+
+    console.log(resolved);
     return {
         props: {
-            allImageData: resolved,
+            allImageData: resolved.data,
         },
     }
 }
@@ -19,24 +21,23 @@ function cn(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Gallery({ allImageData }: { allImageData: FileObject[] }) {
+export default function Gallery({ allImageData }) {
     const [isLoading, setLoading] = useState(true);
     return (
        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24
         sm:px-6 lg:max-w-7xl lg:px-8">
             <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2
             gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                {allImageData.map(({ id, name }) => (
+                {allImageData.map(({ id, name, imageSrc, username, comments }) => (
                     <li key={id}>
                         <Link href={`/images/${name}`}>
                             <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full
                             overflow-hidden rounded-lg bg-gray-200">
                                 <Image
                                     alt=""
-                                    src={`https://mphoimujumzasrbtyzhr.supabase.co/storage/v1/object/public/image-bucket/${name}`}
+                                    src={imageSrc}
                                     width="50"
                                     height="50"
-                                    objectFit="cover"
                                     className={cn(
                                         "group-hover:opacity-75 duration-700 ease-in-out",
                                         isLoading
@@ -46,8 +47,8 @@ export default function Gallery({ allImageData }: { allImageData: FileObject[] }
                                     onLoadingComplete={() => setLoading(false)}
                                 />
                             </div>
-                            <h3 className="mt-4 text-sm text-gray-700">Lucas harlor</h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">@lharlor</p>
+                            <h3 className="mt-4 text-sm text-gray-700">{username}</h3>
+                            <p className="mt-1 text-lg font-medium text-gray-900">{comments}</p>
                         </Link>
                     </li>
                 ))}
