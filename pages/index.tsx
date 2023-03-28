afc307cdf1ca8c4b82d4b18afe00dcc419cb7840
Lucas Harlor <lucasharlor@gmail.com>
2023-03-27 21:21:49 -0700

    Removed image list dots and added a navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import FileObject from 'next/app';
-import { getImageInfo,getAllImageData } from '../lib/images'
+import { getImageInfo } from '../lib/images'
+import { Nav } from '../lib/nav_toolbar';
 import { useState } from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
@@ -24,20 +24,22 @@ function cn(...classes: string[]) {
 export default function Gallery({ allImageData }) {
     const [isLoading, setLoading] = useState(true);
     return (
-       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24
+
+       <div className="max-w-2xl mx-auto py-8 px-4 sm:py-12
         sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2
-            gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <Nav />
+            <div className="grid grid-cols-1 gap-y-5 sm:grid-cols-2
+            gap-x-3 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-4">
                 {allImageData.map(({ id, name, imageSrc, username, comments }) => (
-                    <li key={id}>
+                    <div>
                         <Link href={`/images/${name}`}>
                             <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full
                             overflow-hidden rounded-lg bg-gray-200">
                                 <Image
                                     alt=""
                                     src={imageSrc}
-                                    width="50"
-                                    height="50"
+                                    width="80"
+                                    height="80"
                                     className={cn(
                                         "group-hover:opacity-75 duration-700 ease-in-out",
                                         isLoading
@@ -50,7 +52,7 @@ export default function Gallery({ allImageData }) {
                             <h3 className="mt-4 text-sm text-gray-700">{username}</h3>
                             <p className="mt-1 text-lg font-medium text-gray-900">{comments}</p>
                         </Link>
-                    </li>
+                    </div>
                 ))}
             </div>
         </div> 
