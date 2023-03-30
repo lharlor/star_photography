8eb9da4d0e619a3c88ad925c6c638815047e2caa
Lucas Harlor <lucasharlor@gmail.com>
2023-03-30 15:29:24 -0700

    Enlarged the images and added an on hover

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,23 +25,22 @@ export default function Gallery({ allImageData }) {
     const [isLoading, setLoading] = useState(true);
     return (
 
-       <div className="max-w-2xl mx-auto py-8 px-4 sm:py-12
-        sm:px-6 lg:max-w-7xl lg:px-8">
+       <div>
             <Nav />
             <div className="grid grid-cols-1 gap-y-5 sm:grid-cols-2
             gap-x-3 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-4">
                 {allImageData.map(({ id, name, imageSrc, username, comments }) => (
                     <div>
                         <Link href={`/images/${name}`}>
-                            <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full
+                            <div className="aspect-w-1 aspect-h-1 xl:aspect-w-5 xl:aspect-h-5 w-full
                             overflow-hidden rounded-lg bg-gray-200">
                                 <Image
                                     alt=""
                                     src={imageSrc}
-                                    width="80"
-                                    height="80"
+                                    width="500"
+                                    height="500"
                                     className={cn(
-                                        "group-hover:opacity-75 duration-700 ease-in-out",
+                                        "hover:opacity-70 group-hover:opacity-75 duration-700 ease-in-out",
                                         isLoading
                                             ? 'grayscale blur-2xl scale-110'
                                             : 'grayscale-0 blur-0 scale-100'
@@ -49,8 +48,6 @@ export default function Gallery({ allImageData }) {
                                     onLoadingComplete={() => setLoading(false)}
                                 />
                             </div>
-                            <h3 className="mt-4 text-sm text-gray-700">{username}</h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">{comments}</p>
                         </Link>
                     </div>
                 ))}
