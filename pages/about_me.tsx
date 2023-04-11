c5dbc88e972aff89a9f6acfc45c96b34666bd490
Lucas Harlor <lucasharlor@gmail.com>
2023-04-10 20:50:35 -0700

    Created the basis for the about me and image upload

--- pages/about_me.tsx
+++ pages/about_me.tsx
@@ -1,9 +1,27 @@
+import Image from 'next/image';
+import profilePic from '../assets/me.jpg';
 
 export default function About() {
 
     return (
         <div className="max-w-2xl mx-auto py-8 px-4 sm:py-12
         sm:px-6 lg:max-w-7xl lg:px-8">
+            <h1>About Me-</h1>
+            <div className="max-w-2xl mx-auto py-8 px-2 sm:py-2">
+                <Image
+                    id="profileImage"
+                    alt=""
+                    src={profilePic }
+                    width="200"
+                    height="200"
+                />
+            </div>
+            <div>
+                <p>I am Lucas Harlor, a Software Developer with a passion for astrophotography. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
         </div>
     )
 }
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,10 +1,31 @@
-
+import { useState } from "react";
+import { Input, Button, Textarea, Grid } from '@nextui-org/react';
 
 export default function Upload() {
 
     return (
-        <div className="max-w-2xl mx-auto py-8 px-4 sm:py-12
-        sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="max-w-2xl mx-auto py-10 px-8 sm:py-12
+        sm:px-12 lg:max-w-2xl lg:px-4">
+            <Grid.Container gap={2} justify="center">
+                <Grid md={12} justify="center">
+                    <Grid md={4} justify="center">
+                        <Input bordered placeholder="Image Name" />
+                    </Grid>
+                    <Grid md={4} justify="center">
+                        <Input bordered type="url" />
+                    </Grid>  
+                </Grid>
+                <Grid md={12} justify="center">
+                    <Textarea bordered
+                        minRows={2}
+                        maxRows={10}
+                        size="xl"
+                        placeholder="Comments..." />
+                </Grid>
+                <Grid md={12} justify="center">
+                    <Button type="submit">Submit</Button>
+                </Grid>
+            </Grid.Container>
         </div>
     )
 }
